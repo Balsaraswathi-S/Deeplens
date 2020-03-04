d6a73f6a45bcce22d3b7b1f3926b58bd41f94f1a
Balsaraswathi <balasuraj2829@gmail.com>
2020-03-04 14:36:39 +0530

    deeplens setup

---
+++ lambda/unknown.py
@@ -0,0 +1,52 @@
+import json
+import boto3
+import requests
+import hashlib
+import os 
+from urllib.parse import parse_qs
+
+bucket_name = os.environ['Deep-apk']
+slack_token = os.environ['xoxp-929407804161-941770225558-931346518001-c7e1d9ac7da10763c84094109931a528']
+slack_channel_id = os.environ['CTPNN7N3G']
+rekognition_collection_id = os.environ['deeplensapk']
+
+
+def unknown(event, context):
+    key = event['Records'][0]['s3']['object']['key']
+
+    data = {
+        "channel": slack_channel_id,
+        "text": "I don't know who this is, can you tell me?",
+        "attachments": [
+            {
+                "image_url": "https://s3.amazonaws.com/%s/%s" % (bucket_name, key),
+                "fallback": "Nope?",
+                "callback_id": key,
+                "attachment_type": "default",
+                "actions": [{
+                        "name": "username",
+                        "text": "Select a username...",
+                        "type": "select",
+                        "data_source": "users"
+                    },
+                    {
+                        "name": "discard",
+                        "text": "Ignore",
+                        "style": "danger",
+                        "type": "button",
+                        "value": "ignore",
+                        "confirm": {
+                            "title": "Are you sure?",
+                            "text": "Are you sure you want to ignore and delete this image?",
+                            "ok_text": "Yes",
+                            "dismiss_text": "No"
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+    print(data)
+    foo = requests.post("https://slack.com/api/chat.postMessage", headers={'Content-Type':'application/json;charset=UTF-8', 'Authorization': 'Bearer %s' % slack_token}, json=data)
+
+    print(foo.json())
